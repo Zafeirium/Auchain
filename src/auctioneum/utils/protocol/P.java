d3ba4a65ce08dd7d7897b08a126d6a851f55432f
Zafeirium <34376272+Zafeirium@users.noreply.github.com>
2017-12-16 15:46:06 +0200

    Create P.java

---
+++ src/auctioneum/utils/protocol/P.java
@@ -0,0 +1,33 @@
+package src.auctioneum.utils.protocol;
+
+import java.util.HashMap;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class T {
+    
+    public static final int PORT = 54321;
+    
+    public static String SUCCESS = "1";
+    public static String NOT_SUCCESS = "2";
+    public static String NAME_EXIST = "3";
+    public static String WRONG_CREDENTIALS = "4";
+    public static String WRONG_OTP = "5";
+    public static String OTP_ERROR = "6";
+    public static String PRIVATE_KEY_ACK = "7";
+
+    public static String SIGN_UP = " a";
+    public static String SIGN_UP_CONFIRM = " b";
+
+    public static String LOG_IN = " c";
+    public static String LOG_IN_CONFIRM = " d";
+
+    public static String OTP = " e";
+    public static String OTP_CONFIRM = " f";
+
+    public static String MAIN = " g";
+    public static String MAIN_CONFIRM = " h";
+    
+    public static String PRIVATE_KEY = " i";
+    
+}
